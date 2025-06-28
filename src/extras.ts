cfd3ce51fe9c09c8e6ec5acfa13129c095f36d34
Nils Riedemann <nils@bleepbloop.studio>
2025-06-28 02:59:33 +0200

    feat: make mutate return the new value

--- src/extras.ts
+++ src/extras.ts
@@ -1,14 +1,15 @@
 import { WriteableSignal } from './core.js'
 
 /**
-  * Mutate the value held by a signal and notify its subscribers. Useful when 
-  * storing an expensive value that can be mutated in place. eg. Sets, Maps,
-  * Arrays and the likes.
-  **/
-export const mutate = <T>(self: WriteableSignal<T>, fn: (value: T) => void) => {
+ * Mutate the value held by a signal and notify its subscribers. Useful when
+ * storing an expensive value that can be mutated in place. eg. Sets, Maps,
+ * Arrays and the likes.
+ **/
+export const mutate = <T, R>(self: WriteableSignal<T>, fn: (value: T) => R) => {
   const value = self.$.currentValue
-  fn(value)
+  const ret = fn(value)
   self.$.emit()
+  return ret
 }
 
 /**
